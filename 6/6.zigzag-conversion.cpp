52576131b05523eea83cb12740ec7f61a81f6590
Rimka <rimka.aromaug@gmail.com>
2025-12-23 02:02:04 +0300

    leetcode task 6 solution

--- 6/6.zigzag-conversion.cpp
+++ 6/6.zigzag-conversion.cpp
@@ -70,12 +70,80 @@
  * 
  */
 
+#include <cstdint>
+#include <cstring>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
 // @lc code=start
-class Solution {
+class Solution
+{
+
 public:
-    string convert(string s, int numRows) {
-        
+
+    string convert (string s, int numRows)
+    {
+        string zigZagStr;
+        const uint16_t columnHeight = numRows;
+        const uint16_t diagonalLen = numRows;
+        if (columnHeight == s.length () || columnHeight == 1)
+            return s;
+        const uint16_t oneZigZagElement = columnHeight + diagonalLen - 2;
+        const uint16_t rawColumnCount = s.length () / oneZigZagElement;
+        const uint16_t columnCount = rawColumnCount + (s.length () % oneZigZagElement ? 1 : 0);
+        const uint16_t oneRowLen = columnCount + rawColumnCount * (diagonalLen - 2) + s.length () % oneZigZagElement;
+        uint8_t eachLineSymbol[numRows][oneRowLen];
+        uint8_t eachLineSymbolCount[numRows];
+
+        for (size_t i = 0; i < numRows; i++)
+        {
+            memset (&eachLineSymbol[i][0], 0, sizeof (uint8_t) * oneRowLen);
+            eachLineSymbolCount[i] = 0;
+        }
+
+        uint16_t rowDelta = 0;
+
+        for (size_t i = 1; i <= s.length (); i++)
+        {
+            const uint16_t rawIndex = i % oneZigZagElement;
+            const uint16_t indInOneZigElement = rawIndex ? rawIndex : oneZigZagElement;
+            const bool isCol = indInOneZigElement <= columnHeight;
+
+            uint16_t lineNum;
+
+            if (isCol)
+                lineNum = indInOneZigElement - 1;
+            else
+                lineNum = diagonalLen - (indInOneZigElement - columnHeight) - 1;
+
+            eachLineSymbol[lineNum][eachLineSymbolCount[lineNum]++] = s[i - 1];
+        }
+
+        for (size_t i = 0; i < numRows; i++)
+        {
+            for (size_t j = 0; j < oneRowLen; j++)
+            {
+                if (eachLineSymbol[i][j] == 0)
+                    break;
+
+                zigZagStr.push_back (eachLineSymbol[i][j]);
+            }       
+        }
+
+
+        return zigZagStr;
     }
 };
 // @lc code=end
+
+int main (int argc, char const *argv[])
+{
+    Solution sol;
+    cout << sol.convert ("hjouvsuyoypayulyeimuotehzriicfskpggkbbipzzrzucxamludfykgruowzgiooobppleqlwphapjnadqhdcnvwdtxjbmyppphauxnspusgdhiixqmbfjxjcvudjsuyibyebmwsiqyoygyxymzevypzvjegebeocfuftsxdixtigsieehkchzdflilrjqfnxztqrsvbspkyhsenbppkqtpddbuotbbqcwivrfxjujjddntgeiqvdgaijvwcyaubwewpjvygehljxepbpiwuqzdzubdubzvafspqpqwuzifwovyddwyvvburczmgyjgfdxvtnunneslsplwuiupfxlzbknhkwppanltcfirjcddsozoyvegurfwcsfmoxeqmrjowrghwlkobmeahkgccnaehhsveymqpxhlrnunyfdzrhbasjeuygafoubutpnimuwfjqsjxvkqdorxxvrwctdsneogvbpkxlpgdirbfcriqifpgynkrrefx", 503) << endl;
+    return 0;
+}
 
