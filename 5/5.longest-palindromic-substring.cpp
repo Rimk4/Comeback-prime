25b171511a0ab66b6ef114b0cf4ce2faf0cba8b5
Rimka <rimka.aromaug@gmail.com>
2025-12-22 22:19:03 +0300

    refactor: try by myself 5 task

--- 5/5.longest-palindromic-substring.cpp
+++ 5/5.longest-palindromic-substring.cpp
@@ -45,57 +45,57 @@
 #include <iostream>
 #include <cstring>
 
-#include <algorithm>
-#include <iterator>
-#include <vector>
-
 using namespace std;
 
 
-class Solution {
+class Solution
+{
+
 public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        if (n <= 1) return s;
-        
-        // Create a DP table to store palindrome info
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        
-        // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+
+    string longestPalindrome (string s)
+    {
+        bool isPrevSubstringPalindrom[s.length ()][s.length ()];
+
+        for (size_t i = 0; i < s.length (); i++)
+        {
+            memset (&isPrevSubstringPalindrom[i][0], 0, s.length () * sizeof (bool));
+            isPrevSubstringPalindrom[i][i] = true;
         }
-        
-        int start = 0;  // Start index of longest palindrome
-        int maxLength = 1;  // Length of longest palindrome
-        
-        // Check for substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
+
+        int maxLen = 1;
+        int start = 0;
+        int end = 0;
+
+        for (size_t i = 0; i < s.length () - 1; i++)
+        {
+            if (s[i] == s[i + 1])
+            {
+                isPrevSubstringPalindrom[i][i + 1] = true;
+                maxLen = 2;
                 start = i;
-                maxLength = 2;
             }
         }
-        
-        // Check for lengths greater than 2
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;  // Ending index
-                
-                // Check if the substring is palindrome
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    
-                    if (len > maxLength) {
-                        start = i;
-                        maxLength = len;
+
+        for (size_t len = 3; len <= s.length (); len++)
+        {
+            for (size_t indStart = 0; indStart <= s.length () - len; indStart++)
+            {
+                size_t indEnding = indStart + len - 1;
+                if (s[indStart] == s[indEnding] && isPrevSubstringPalindrom[indStart + 1][indEnding - 1])
+                {
+                    isPrevSubstringPalindrom[indStart][indEnding] = true;
+   
+                    if (maxLen < len)
+                    {
+                        maxLen = len;
+                        start = indStart;
                     }
                 }
             }
         }
-        
-        return s.substr(start, maxLength);
+
+        return s.substr (start, maxLen);
     }
 };
 
@@ -103,7 +103,7 @@ int main (int argc, char const *argv[])
 {
     Solution sol;
     string str ("123234qwertytrewqasddsa");
-    string str1 ("bb");
-    cout << sol.longestPalindrome (str) << endl;
+    string str1 ("aacabdkacaa");
+    cout << sol.longestPalindrome (str1) << endl;
     return 0;
 }
